24ff3a904f131eca7f4afe689c5ea340eaf10435
argostellar <arskud@gmail.com>
2020-09-12 21:20:20 +0700

    Выполняет задание 4. Учебный проект: время разбивать камни

---
+++ src/view/board-sorting.js
@@ -0,0 +1,7 @@
+export const createBoardSortingTemplate = () => {
+  return (
+    `<a href="#" class="board__filter">SORT BY DEFAULT</a>
+          <a href="#" class="board__filter">SORT BY DATE up</a>
+          <a href="#" class="board__filter">SORT BY DATE down</a>`
+  );
+};
