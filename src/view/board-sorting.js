a7481beaacd69a8db0d4a2c11dd38ea37683cb99
argostellar <arskud@gmail.com>
2020-09-17 10:57:50 +0700

    Выполняет учебное задание к лекции 4

--- src/view/board-sorting.js
+++
@@ -1,7 +0,0 @@
-export const createBoardSortingTemplate = () => {
-  return (
-    `<a href="#" class="board__filter">SORT BY DEFAULT</a>
-          <a href="#" class="board__filter">SORT BY DATE up</a>
-          <a href="#" class="board__filter">SORT BY DATE down</a>`
-  );
-};
